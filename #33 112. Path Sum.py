230ec04e92eaac4baefafc842557a7d2ec23a3d8
TheKaveri <114827346+TheKaveri@users.noreply.github.com>
2024-06-07 20:38:20 -0230

    Add files via upload

--- #33 112. Path Sum.py
+++ #33 112. Path Sum.py
@@ -54,4 +54,3 @@ def has_path_sum(root: Optional[TreeNode], target_sum: int) -> bool:
         return root.val == target_sum
     
     return has_path_sum(root.left, target_sum - root.val) or has_path_sum(root.right, target_sum - root.val)
-
